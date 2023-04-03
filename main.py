e350743f33ed003be7b9799b11e6dc2914226742
Chris Whitmer <tru_x1@msn.com>
2023-04-03 10:40:28 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
-import random
+import os
 import smtplib
 from email.message import EmailMessage
-from quote_list import get_quotes
-from notes import read_note
-import markdown
+from file_locator import FileLocator
+import keyring
+
+os.chdir('/home/chris/Desktop/DailyQuotes')
 
 
-def get_contacts():
-  """Parse contact list to retrieve the name and email address of each person that wants an email"""
+def get_contacts() -> list[str]:
     addresses = []
 
-    with open('contacts.txt', 'r') as contacts:
+    with open(r'/home/chris/Desktop/DailyQuotes/contacts.txt', 'r') as contacts:
         addresses = [contact.strip() for contact in contacts]
 
     contacts = [line.split(', ')[0] for line in addresses]
@@ -19,38 +19,39 @@ def get_contacts():
     return contacts, emails
 
 
-def send(contacts: list, emails: list):
-  """Sending of the email to each user gathered from the contact list"""
+def send(contacts: list, emails: list, note: str, quote: dict):
     for c, e in zip(contacts, emails):
         contact = c.strip()
         email = e.strip()
 
         msg = EmailMessage()
         msg['Subject'] = "Random Quote and Note"
-        msg['From'] = 'email'
+        msg['From'] = 'dailyquotegenerator@gmail.com'
         msg['To'] = email
         msg.set_content("Plain Text")
         msg.add_alternative(f"""\
             <!DOCTYPE html>
             <html>
                 <body>
-                    <p>Hi {contact}, Here is your quote and note of the day.</p>
+                    <p>Hi {contact}, Here is your quote and note of the day.</p></br>
                     <p>Quote by: <b>{quote['author']}</b></p>
                     <p><i>{quote['quote']}</i></p>
                     <br>
-                    <p>{markdown_note}</p>
+                    <p>{note}</p>
                 </body>
             </html>
             """, subtype='html')
 
         with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
-            smtp.login('email', 'password')
+            smtp.login(keyring.get_password('gmail', 'email'),
+                       keyring.get_password('gmail', 'password'))
             smtp.send_message(msg)
 
 
 if __name__ == '__main__':
-    quote = random.choice(get_quotes())
-    note = read_note()
-    markdown_note = markdown.markdown(note)
+    locator = FileLocator()
+    note = locator.get_content(path='200 - Citadel', quote=False)
+    quote = locator.get_content(
+        path='400 - Archive/300 - Literature Notes', quote=True)
     contacts, emails = get_contacts()
-    send(contacts, emails)
+    send(contacts, emails, note, quote)
